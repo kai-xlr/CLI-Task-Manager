6903f85bb6b9cc022077282260a88e5117f6b92c
kai-xlr <xhauma@protonmail.com>
2025-09-24 22:27:54 -0400

    2. Adding comments to todo.go

--- internal/todo/todo.go
+++ internal/todo/todo.go
@@ -1,10 +1,13 @@
+// Package todo provides a simple todo list implementation
 package todo
 
+// Item represents a todo item with text and completion status
 type Item struct {
 	Text string
 	Done bool
 }
 
+// NewItem creates a new todo item with the specified text
 func NewItem(text string) Item {
 	return Item{
 		Text: text,
